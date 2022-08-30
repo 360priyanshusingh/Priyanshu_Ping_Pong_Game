36882c01ba22e8bc92ee736846d1a2e851d1a389
Priyanshu Sisodiya <upscpriyanshu06@gmail.com>
2022-08-30 18:40:09 +0530

    c2

--- Ping_Pong.js
+++ Ping_Pong.js
@@ -1,19 +1,20 @@
- 
- function  fetchingrandomDogimg (){
-    var  xhrRequest = new  XMLHttpRequest();
-    xhrRequest.onload=function(){
-       
-        var responseJSON = JSON.parse(xhrRequest.response);
-       
-        var imgurl=responseJSON.message;
-        console.log(imgurl);
 
-        $('#dog-img').attr('src',imgurl);
-    }
-
-    xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',TextTrackCue)
-    xhrRequest.send();
+ function desplay(data){
+    $('<img>',{
+    src: data.url ,
+    hight : '100%',
+    width : '100%',
+    }).appendTo('#img-container');
+    console.log(data.url);
+    // $(document.createElement('img')).attr('src',data.url).appendTo('#img-container');
 
 }
-
-$('#fetch-dog-img').click(fetchingrandomDogimg);
+$.ajax({
+    url :'https://api.nasa.gov/planetary/apod',
+    method :'GET',
+    success: desplay,
+    data :{
+        api_key:'DEMO_KEY',
+        date:'2018-06-07',
+    },
+})
